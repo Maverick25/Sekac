e7c314b1705d750c61a7bd6a2d2655239e0d518e
Marek Rigan <rigan.marek@gmail.com>
2016-03-14 00:11:48 +0100

    attempt to upload file to Dropbox

--- app/src/main/java/agency/alterway/sekac/controllers/FileController.java
+++ app/src/main/java/agency/alterway/sekac/controllers/FileController.java
@@ -3,8 +3,16 @@ package agency.alterway.sekac.controllers;
 import android.os.Environment;
 import android.util.Log;
 
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.users.FullAccount;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,12 +30,18 @@ import au.com.bytecode.opencsv.CSVWriter;
  */
 public class FileController
 {
+    private static final String ACCESS_TOKEN = "Hbm6EwWgYu4AAAAAAAAL8rfitsCPW1mvQzJ1FbovWRN_RE0HWfrCgN-Hnp7H1mZ5";
     private static FileController instance;
     private static Injection injection;
+    private DbxClientV2 client;
 
     private FileController(Injection injection)
     {
         FileController.injection = injection;
+
+        // Create Dropbox client
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+         client = new DbxClientV2(config, ACCESS_TOKEN);
     }
 
     public static FileController getInstance(Injection injection)
@@ -44,6 +58,23 @@ public class FileController
         return instance;
     }
 
+    private void uploadToDropbox(String filename)
+    {
+        // Upload "test.txt" to Dropbox
+        try
+        {
+            // Get current account info
+            FullAccount account = client.users().getCurrentAccount();
+            System.out.println(account.getName().getDisplayName());
+
+            InputStream in = new FileInputStream(filename);
+            client.files().uploadBuilder("/"+filename).uploadAndFinish(in);
+        }
+        catch (DbxException | IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     public String exportToCSV(Date selectedDate, List<Cut> treeCuts, Summary summary)
     {
@@ -57,7 +88,8 @@ public class FileController
 
         try
         {
-            File file = new File(exportDir.getAbsolutePath()+"/sekac_" + DateHandler.fileFormatter.format(selectedDate) + ".csv");
+            String filename = "sekac_" + DateHandler.fileFormatter.format(selectedDate) + ".csv";
+            File file = new File(exportDir.getAbsolutePath()+"/"+filename);
 
             if (file.exists())
             {
@@ -101,6 +133,8 @@ public class FileController
                 writer.writeAll(data);
 
                 writer.close();
+
+            uploadToDropbox(filename);
 
             return "Súbor bol úspešne vytvorený";
         }
